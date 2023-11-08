0189aba47c41b40a22bde8dfb4c1b47ff6d00717
Julie <raymondjulie@proton.me>
2023-11-08 15:23:59 +0100

    views files order changed and oragnisation of controller manager

--- src/Model/ArticleManager.php
+++ src/Model/ArticleManager.php
@@ -8,44 +8,61 @@ class ArticleManager extends AbstractManager
 {
     public const TABLE = 'article';
 
-    public function getAllArticlesWithAuthors(): array
+    public function addArticle(array $data)
     {
-        $query = "SELECT A.*, BU.name AS author_name
-                  FROM article A
-                  INNER JOIN blog_user BU ON A.blog_user_id = BU.id";
-
-        $result = $this->pdo->query($query)->fetchAll(PDO::FETCH_ASSOC);
-
-        return $result;
-    }
-
-    public function insert(array $data): int
-    {
-        $query = "INSERT INTO " . self::TABLE . " (title, content, image, blog_user_id) 
-                  VALUES (:title, :content, :image, :blog_user_id)";
+        $query = 'INSERT INTO ' . static::TABLE . ' (title, content, image, blog_user_id) VALUES (:title, :content, :image, :blog_user_id)';
         $statement = $this->pdo->prepare($query);
-        $statement->execute($data);
-
-        return (int) $this->pdo->lastInsertId();
-    }
-
-    public function update(array $data): void
-    {
-        $query = "UPDATE " . self::TABLE . " SET title = :title, content = :content, image = :image WHERE id = :id";
-        $statement = $this->pdo->prepare($query);
-        $statement->execute($data);
-    }
-
-    public function delete(int $id): void
-    {
-        $query = "DELETE FROM " . self::TABLE . " WHERE id = :id";
-        $statement = $this->pdo->prepare($query);
-        $statement->bindValue(':id', $id, \PDO::PARAM_INT);
+        $statement->bindValue(':title', $data['title']);
+        $statement->bindValue(':content', $data['content']);
+        $statement->bindValue(':image', $data['image']);
+        $statement->bindValue(':blog_user_id', $data['blog_user_id'], \PDO::PARAM_INT);
         $statement->execute();
     }
 
-    public function getLastInsertedId(): int
+    public function editArticle(int $articleId, array $data)
     {
-        return (int) $this->pdo->lastInsertId();
+        $query = 'UPDATE ' . static::TABLE . ' SET title = :title, content = :content, image = :image WHERE id = :id';
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':title', $data['title']);
+        $statement->bindValue(':content', $data['content']);
+        $statement->bindValue(':image', $data['image']);
+        $statement->bindValue(':id', $articleId, \PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+    public function deleteArticle(int $articleId)
+    {
+        $query = 'DELETE FROM ' . static::TABLE . ' WHERE id = :id';
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':id', $articleId, \PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+    public function getArticlesByUserId(int $userId)
+    {
+        $query = 'SELECT * FROM ' . static::TABLE . ' WHERE blog_user_id = :user_id';
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':user_id', $userId, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
+
+    public function getAllArticles()
+    {
+        $query = "SELECT A.*, BU.name AS author_name
+        FROM article A
+        INNER JOIN blog_user BU ON A.blog_user_id = BU.id";
+        return $this->pdo->query($query)->fetchAll();
+    }
+
+    public function getArticleById(int $articleId)
+    {
+        $query = 'SELECT * FROM ' . static::TABLE . ' WHERE id = :id';
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':id', $articleId, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetch();
     }
 }
