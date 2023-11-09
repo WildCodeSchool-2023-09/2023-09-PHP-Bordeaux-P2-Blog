b987cc59d5f2bf2adbefef9eb71cc61269280d7a
ComeSarazin <sasacoco@orange.fr>
2023-11-09 17:18:17 +0100

    création de la branche S-3-Pagecontact/propos,rajout de la function propos dans PageController et création de porpos.html.twig

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -10,4 +10,9 @@ class PageController extends AbstractController
     {
         return $this->twig->render('Home/confidentialite.html.twig');
     }
+
+    public function propos(): string
+    {
+        return $this->twig->render('Home/propos.html.twig');
+    }
 }
