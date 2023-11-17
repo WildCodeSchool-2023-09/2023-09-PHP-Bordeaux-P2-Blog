f547fb202f4c45d992d6ea8c00efae631164cb13
Julie <raymondjulie@proton.me>
2023-11-17 16:16:36 +0100

    fix récupération de l'user id pour la page de vue forgot_password

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -139,7 +139,7 @@ class ProfilController extends AbstractController
                 exit();
             }
         }
-
-        echo $this->twig->render('Blog_user/forgot_password.html.twig');
+        $userId = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
+        echo $this->twig->render('Blog_user/forgot_password.html.twig', ['userId' => $userId]);
     }
 }
